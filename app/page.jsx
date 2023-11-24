f30f65b61943b1c9f68ef72dbc11ac32037c0aab
Pixel10312 <115668052+carsonwng@users.noreply.github.com>
2023-11-24 15:16:28 -0500

    barely running init

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+// import { Heading } from '@carbon/react'
+// import './st'
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Heading> */}
+        Log in.
+      {/* </Heading> */}
+    </div>
+  )
+}
+
+export default Home
---
+++ app/student/page.jsx
@@ -0,0 +1,9 @@
+const StudentPage = () => {
+    return (
+        <div>
+            Hi
+        </div>
+    )
+}
+
+export default StudentPage
